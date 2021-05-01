073d77ec011fadb01d7b0cf2b006a1b8e8b4439e
Miley Dawn <mileydawn@huongs-mbp.myfiosgateway.com>
2021-05-01 02:13:11 -0400

    create a test component to implement Material UI

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Login from './components/login/Login.js'
 import Favorites from './components/favorites/Favorites.js'
 import Directions from './components/directions/Directions.js'
 import PageNotFound from './components/pageNotFound/PageNotFound.js'
+import Test from './components/test/Test.js'
 
 export default function App(props) {
   return (
@@ -16,6 +17,7 @@ export default function App(props) {
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/directions" component={Directions} />
+        <Route exact path="/test" component={Test} />
         <Route path="/" component={PageNotFound} />
       </Switch>
     </Router>
